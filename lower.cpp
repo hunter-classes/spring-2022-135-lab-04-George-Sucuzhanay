e7ed631923aa0f33899b160f7d8fb5bd4fa7447b
George <georgesucuzhanay100@gmail.com>
2022-02-24 09:57:23 -0500

    created a nested loop that create a upper triangle for task e

--- lower.cpp
+++ lower.cpp
@@ -4,6 +4,14 @@
 
 std::string lower(int length)
 {
-    std::string test = "Hello World!\n";
-    return test;
+    std::string result = "";
+    for(int i =0; i < length; i++)
+    {
+        for(int j=0; j < i + 1; j++){
+            result += "*";
+        }
+        result += "\n";
+
+    }
+    return result;
 }
